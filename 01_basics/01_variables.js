fc26ceaa1ae98247ffc02b4a2800e24aa2f647a3
Razan Bhandari <107500071+Mickey4541@users.noreply.github.com>
2024-01-08 07:28:29 +0000

    01_variables commit successfully

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,12 +2,18 @@ const account_Id = 4541
 let accountEmail = "rajan@gmail.com"
 var accountPassword = "12345"
 accountCity = "Dang"
+let accountState;
 
 //account_Id = 2  not allowed(const cannot be changed.)
 
 accountEmail = "bhandari@gmail.com"
 accountCity = "Rolpa"
 accountPassword = "2580"
-console.log(account_Id)
+console.log(account_Id);
 
-console.table([accountEmail,accountPassword,account_Id, accountCity]) //to print in table structure at once.
+/*
+prefer not to use var because
+of issue in block scope and functional scope.
+*/
+
+console.table([accountEmail,accountPassword,account_Id, accountCity, accountState]) //to print in table structure at once.
