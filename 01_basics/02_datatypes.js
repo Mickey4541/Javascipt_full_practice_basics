fe1338b21c77c4f8024820a51fbf6ae3cb3464a0
Razan Bhandari <107500071+Mickey4541@users.noreply.github.com>
2024-01-08 08:10:40 +0000

    datatypes commit successfully

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+//alert(3+3) //we are using node js, not browser
+
+console.log(3+
+    3)        //code readability is not correct
+
+console.log("Rajan") //code readability must be high.
+
+let name = "Rajan"//string
+let age = 18 // chat
+let isLoggedIn = true//boolean
+let state = null; //null in used like in ex: temperature.
+
+//number range which can store in one variable = 2 to the power 53
+//bigint = used in big it ocmpanies.
+//string = "Rajan"
+//null = standalone value
+//undefined = 
+//symbol = unique
+//object
+
+console.log(typeof age); //type is number
+
+console.log(typeof null); //type of null is object
