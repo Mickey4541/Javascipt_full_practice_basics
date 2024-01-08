505aa53e8b5cdc5b784c5c78dbf11b75a7000ddc
Razan Bhandari <107500071+Mickey4541@users.noreply.github.com>
2024-01-08 07:15:11 +0000

    01_variables commit successfully

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const account_Id = 4541
+let accountEmail = "rajan@gmail.com"
+var accountPassword = "12345"
+accountCity = "Dang"
+
+//account_Id = 2  not allowed(const cannot be changed.)
+
+accountEmail = "bhandari@gmail.com"
+accountCity = "Rolpa"
+accountPassword = "2580"
+console.log(account_Id)
+
+console.table([accountEmail,accountPassword,account_Id, accountCity]) //to print in table structure at once.
