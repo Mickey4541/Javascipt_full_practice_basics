40d3e2018d05fa81c6bdcab9507f31316638745e
Razan Bhandari <107500071+Mickey4541@users.noreply.github.com>
2024-01-10 09:17:08 +0000

    04_comparision commited successfully.

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,19 @@
+//console.log(2>1);
+//console.log(2>=1);
+//console.log(2<1);
+//console.log(2<=1);
+
+console.log("2" > 1);//true
+//following code lai jati sakyo avoid garnu parchha to improve clean code and readability.
+console.log(null > 0);//false
+console.log(null == 0);//false
+console.log(null >= 0);//true
+
+
+console.log(undefined == 0);//false
+console.log(undefined > 0);//false
+console.log(undefined < 0);//false
+
+
+
+console.log("2" === 2);// fasle //this triple equals to strictly check the value and datatypes of the given number.
