270eee97458ca3d129876e83f4edd194a521c93f
Razan Bhandari <107500071+Mickey4541@users.noreply.github.com>
2024-05-29 15:49:10 +0545

    maths and numbers in js
    maths and numbers in js

---
+++ 01_basics/06_numbers_and_maths.js
@@ -0,0 +1,44 @@
+// const score = 400
+// console.log(score)
+
+// const balance = new Number(100)
+// console.log(balance); //output : [Number: 100]
+
+// console.log(balance.toString().length) //3 
+// console.log(balance.toFixed(2)) //100.00
+
+// const otherNumber = 23.8999
+// console.log(otherNumber.toPrecision(3)) //23.9
+
+// const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'));//1,00,000
+
+
+/* go on colsole:
+type number and enter you can see native code ....
+enter and type Number.(dot) , then there is many more options
+like max value, min value and max safe integer*/
+
+
+// ===================MAths>>>>>>>>>>>>>>>
+
+// console.log(Math.PI); do it in console.
+console.log(Math.abs(-4));/* Absolute value*/ //Output = 4;
+console.log(Math.round(4.7)) // output = 5
+console.log(Math.ceil(4.7)) // output = 5
+console.log(Math.floor(4.7)) // output = 4
+console.log(Math.random()) // output = 0 to 1 in points
+
+
+console.log((Math.random()*10)+1) 
+console.log(Math.floor(Math.random()*10)+1) //last maa 1 add garako 0 na aawos vanna ko lagi ho. tesle 0 to 9 samma ko value dinxa
+
+
+const min = 10
+const max = 20
+
+/*this is most important line. */
+console.log(Math.floor(Math.random()*(max-min+1)+min)) //output = 10 to 20
+
+
+
