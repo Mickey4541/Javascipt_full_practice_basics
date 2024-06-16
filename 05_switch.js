d233e242690914c845d91a7315c2aa5788d6673b
rajanbhandari4541@gmail.com <rajanbhandari4541@gmail.com>
2024-06-16 23:05:43 +0545

    updated

---
+++ 05_switch.js
@@ -0,0 +1,59 @@
+//syntax of switch:::::::::
+// switch(key){
+//     case value:
+//     //code to be executed;
+//     break;
+
+//     case value:
+//     //code to be executed;
+//     break;
+
+//     default:
+//         //default code to be executed.
+//         break;
+// }
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// const month = 1
+const month = "December"
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("August");
+        break;
+    case 9:
+        console.log("September");
+        break;
+    case 10:
+        console.log("October");
+        break;
+    case 11:
+        console.log("November");
+        break;
+    case "December":
+        console.log("December");
+        break;
+    default:
+        console.log("Invalid month");
+}
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
