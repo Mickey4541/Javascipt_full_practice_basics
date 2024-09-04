56e9d2787e65ef443eb5efebf29f960fdee0d2c0
rajanbhandari4541@gmail.com <rajanbhandari4541@gmail.com>
2024-09-04 22:16:03 +0545

    myclasses completed successfully

--- 12_classes_and_oops/myclasses.js
+++ 12_classes_and_oops/myclasses.js
@@ -9,6 +9,31 @@ class User{
     encryptPassword(){
         return `${this.password}abc`
     }
+    changeUSername(){
+        return `${this.username.toUpperCase()}`
+    }
 }
 const coffee = new User("coffeee", "coffee@gmail.com", "123")
 console.log(coffee.encryptPassword());
+console.log(coffee.changeUSername());
+
+
+//behind the scene
+class anotherUser {
+    constructor(username, email, password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+    encryptPassword() {
+        return `${this.password}abc`;
+    }
+    changeUsername() {
+        return `${this.username.toUpperCase()}`;
+    }
+}
+
+
+const tea = new anotherUser("tea", "coffee@gmail.com", "123")
+console.log(tea.encryptPassword());
+console.log(tea.changeUsername());
